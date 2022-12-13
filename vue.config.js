7821684fb5a0afd8acefaef7fc7f85997879d121
HiBixby <201910789@sangmyung.kr>
2022-12-13 21:22:26 +0900

    🚀 Change public path and assets dir

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir:'./docs',
-})
+  outputDir: "./docs",
+  publicPath: "./",
+  assetsDir: "./",
+});
