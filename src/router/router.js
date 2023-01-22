a93b8f61f4b3db1bfd872df42558c40ac7c0c442
HiBixby <201910789@sangmyung.kr>
2023-01-22 12:14:02 +0900

    🎨 페이지 컴포넌트는 이름 뒤에 page 붙이도록 이름 변경

--- src/router/router.js
+++ src/router/router.js
@@ -1,19 +1,16 @@
-import {
-  createRouter,
-  createWebHashHistory,
-} from "vue-router";
-import AddTodo from "../components/AddTodo.vue";
-import Home from "../components/Home.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import TodoPage from "../components/TodoPage.vue";
+import MainPage from "../components/MainPage.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     // path별 component를 추가한다.
-    { path: "/", name: "home", component: Home },
+    { path: "/", name: "home", component: MainPage },
     {
       path: "/todo",
       name: "todo",
-      component: AddTodo,
+      component: TodoPage,
     },
   ],
 });
