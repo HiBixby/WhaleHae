af0b95f7773aa2496403ebe606cd1aa44f0e9f3b
HiBixby <201910789@sangmyung.kr>
2023-02-04 01:10:02 +0900

    ✨ 오래된 알림은 무시하고 몇개의 알림이 무시되었는지 알려주기

--- public/background.js
+++ public/background.js
@@ -1,25 +1,68 @@
 console.log("background.js loaded!");
 
-chrome.alarms.onAlarm.addListener((alarm) => {
-  console.log("Alarm!!!");
-  const id = alarm.name;
-  chrome.storage.local.get(["todos"]).then((result) => {
-    for (index in result.todos) {
-      if (id === result.todos[index].id.toString()) {
-        const message = result.todos[index].title;
-        chrome.notifications.create(id.toString(), {
-          type: "basic",
-          iconUrl: "logo.png",
-          title: "웨, 일해",
-          message: message ? message : "🐳제목 없음🐳",
-          priority: 2,
-        });
-        if (result.todos[index].link !== null) {
-          chrome.tabs.create({
-            url: result.todos[index].link,
-          });
-        }
-      }
+let passedAlarmCnt = 0;
+const CreateTab = (link) => {
+  //링크가 null이 아니라면 탭 열기
+  if (link !== null) {
+    chrome.tabs.create({
+      url: link,
+    });
+  }
+};
+const CreateNotificationOptions = (message) => {
+  let notificationOptions = {
+    type: "basic",
+    iconUrl: "logo.png",
+    title: "웨, 일해",
+    message: "🐳제목없음🐳",
+    priority: 2,
+  };
+  if (message !== null) {
+    notificationOptions.message = message;
+  }
+  return notificationOptions;
+};
+const CreateNotification = (NOTIFICATION_ID, message) => {
+  chrome.notifications.create(
+    NOTIFICATION_ID,
+    CreateNotificationOptions(message)
+  );
+};
+const UpdateNotification = (NOTIFICATION_ID, message) => {
+  chrome.notifications.update(
+    NOTIFICATION_ID,
+    CreateNotificationOptions(message),
+    (wasUpdated) => {
+      console.log("wasUpdated:", wasUpdated);
     }
-  });
+  );
+};
+const FindIndexOfTodo = (id, result) => {
+  for (index in result.todos) {
+    if (id === result.todos[index].id.toString()) return index;
+  }
+};
+
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+  console.log("Alarm executed!", alarm);
+  const id = alarm.name;
+  const result = await chrome.storage.local.get(["todos"]);
+  console.log("result is...", result);
+  const index = FindIndexOfTodo(id, result);
+  //시간이 지난 알람일 때
+  if (Date.now() > result.todos[index].date + 60000) {
+    passedAlarmCnt++;
+    const NOTIFICATION_ID = "hasPassedAlarms";
+    const message = `자리를 비운 동안 ${passedAlarmCnt}개의 알림이 실행되지 않았습니다.`;
+    if (passedAlarmCnt === 1) {
+      CreateNotification(NOTIFICATION_ID, message);
+    } else {
+      UpdateNotification(NOTIFICATION_ID, message);
+    }
+    //정상 알람일 때
+  } else {
+    const message = result.todos[index].title;
+    CreateNotification(id, message);
+    CreateTab(result.todos[index].link);
+  }
 });
