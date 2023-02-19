bc68ae579be88baf7e8c248d7952ef316eed5362
HiBixby <201910789@sangmyung.kr>
2023-02-20 08:43:50 +0900

    🔧 Apply a vite-svg-loader

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
 const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base:'./',
+  plugins: [vue(), svgLoader()],
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
